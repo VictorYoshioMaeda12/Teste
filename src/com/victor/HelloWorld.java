0c53da87786bce03653a830a7694edc4d5422bd5
VictorYoshioMaeda12 <victor.yoshio12@hotmail.com>
2021-11-26 18:45:34 -0300

    First Commit Test

---
+++ src/com/victor/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.victor;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Teste");
+	}
+
+}
